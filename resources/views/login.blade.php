8e283862c7b6624cda065921bb841e49c7a7488e
wislem <peter.lazaridis@gmail.com>
2015-03-27 15:52:58 +0200

    Corrected assets path (vendor instead of package)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,11 +9,11 @@
     <title>{{Config::get('scaffenger.config.title')}}</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{!! asset('packages/wislem/scaffenger/css/bootstrap.css') !!}" rel="stylesheet">
+    <link href="{!! asset('vendor/wislem/scaffenger/css/bootstrap.css') !!}" rel="stylesheet">
 
     <!-- Custom CSS -->
 
-    <link href="{!! asset('packages/wislem/scaffenger/vendor/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet">
+    <link href="{!! asset('vendor/wislem/scaffenger/vendor/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
