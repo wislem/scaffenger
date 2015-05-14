273f11343e16dc8d6992ae21da3a4a0124d267fe
wislem <peter.lazaridis@gmail.com>
2015-05-14 14:19:34 +0300

    Oops... Custom controller means it has to go outside

--- src/Http/Controllers/CustomController.php
+++ src/Http/Controllers/CustomController.php
@@ -11,13 +11,8 @@ class CustomController extends Controller {
     $this->middleware('scaffenger.auth');
   }
 
-  public function getActiveSubscriptions(){
-    $active_customers = DB::table('users')
-                          ->join('subscription_user', 'user_id', '=', 'subscription_user.user_id')
-                          ->where('subscription_user.is_active', '=', 1)
-                          ->get();
-
-    dd($active_customers);
+  public function getIndex(){
+    return 'custom index';
   }
 
 }
